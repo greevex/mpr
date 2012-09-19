a73125d2d2c93829c48644cca00a6b61d4c95e6c
Ostrovskiy Grigoriy <greevex@gmail.com>
2012-09-19 15:56:09 +0400

    Build with LICENSE

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-tar -vczf mpr_client-1.1.src.tar.gz client/*
-tar -vczf mpr_server-1.1.src.tar.gz server/*
-tar -vczf mpr-1.1.src.tar.gz server/ client/
+tar -vczf mpr_client-1.1.src.tar.gz client/* LICENSE
+tar -vczf mpr_server-1.1.src.tar.gz server/* LICENSE
+tar -vczf mpr-1.1.src.tar.gz server/ client/ LICENSE
